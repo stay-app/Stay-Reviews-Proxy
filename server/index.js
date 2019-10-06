ec8e72e2274de23e86be3fd23b04efcd66468f0e
Jinjing <bijinjing88@gmail.com>
2019-10-06 00:46:16 -0700

    update the html for inactive component

--- server/index.js
+++ server/index.js
@@ -1,24 +1,12 @@
 const express = require('express')
 const request = require('request')
 const app = express()
-const port = 3000
-
+const port = 3000 
 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  next();
-});
-
-app.get('/api/reviews', (req, res) => {
-  request(`http://localhost:5002/api/reviews/${req.query.host}`, (err,response,body) => {
-    res.send(JSON.parse(response.body))
-  })
-})
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
